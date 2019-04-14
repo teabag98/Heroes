82631d3450b33383ad7ffa01dd94e79222c013fb
eric <eric.kingoo2014@gmail.com>
2019-04-15 02:59:08 +0300

    add hero parameters

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,5 +1,11 @@
 public class Hero {
     private String mName;
+    private int mAge;
+    private String mWeakness;
+    private String mPower;
+    private int mId;
+    private static List<Hero> instances = new ArrayList<Hero>();
+
 
     public Hero(String name){
         mName=name;
