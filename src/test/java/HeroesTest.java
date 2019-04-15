479bfe32fa1f2ccd8d7104aab4177654a32cdfba
eric <eric.kingoo2014@gmail.com>
2019-04-15 04:25:36 +0300

    add new styles

--- src/test/java/HeroesTest.java
+++ src/test/java/HeroesTest.java
@@ -4,9 +4,21 @@ import static org.junit.Assert.*;
 public class HeroesTest {
 
     @Test
-   public void Hero_instenciatesHero_String(){
-        Hero myHero = new Hero("eric");
+   public void Hero_instenciatesHeroCorrectly_true(){
+        Hero myHero = new Hero("eric",19,"thinker","human");
+        assertEquals(true,myHero instanceof Hero );
+
+    }
+    @Test
+    public void Hero_instanciatesName_String(){
+        Hero myHero = new Hero("eric",19,"thinker","human");
         assertEquals("eric",myHero.getName());
 
     }
+    @Test
+    public void Hero_instantiatesAge_Int(){
+        Hero myHero = new Hero("eric",29,"thinker","human");
+        assertEquals(29, myHero.getAge());
+    }
+
 }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,2 +1,25 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
 public class SquadTest {
+    @Test
+    public void Squad_instantiatesCorrectly_true() {
+        Squad mySquad = new Squad("Wakanda", 5, "protection");
+        assertEquals(true, mySquad instanceof Squad);
+    }
+
+    @Test
+    public void Squad_instantiatesWithName_String() {
+        Squad mySquad = new Squad("wakanda", 5, "protection");
+        assertEquals("Avengers", mySquad.getName());
+    }
+
+    @Test
+    public void Squad_instantiatesWithSize_Int() {
+        Squad mySquad = new Squad("wakanda", 5, "protection");
+        assertEquals(5, mySquad.getSize());
+    }
+
 }
