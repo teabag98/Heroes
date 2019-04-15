034bcde09216a2d5f6edf9af2366cd729503a6f3
eric <eric.kingoo2014@gmail.com>
2019-04-15 03:05:26 +0300

    fix indentation

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,3 +1,8 @@
+
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Hero {
     private String mName;
     private int mAge;
@@ -7,12 +12,40 @@ public class Hero {
     private static List<Hero> instances = new ArrayList<Hero>();
 
 
-    public Hero(String name){
-        mName=name;
+    public Hero(String name, int age, String strength, String weakness) {
+        mName = name;
+        mAge = age;
+        mPower = strength;
+        mWeakness = weakness;
+        instances.add(this);
+        mId = instances.size();
     }
 
     public String getName(){
         return mName;
+    }
+    public int getAge() {
+        return mAge;
+    }
+
+    public String getPower() {
+        return mPower;
+    }
+
+    public String getWeakness() {
+        return mWeakness;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public static List<Hero> all() {
+        return instances;
+    }
+
+    public static void clear() {
+        instances.clear();
     }
 
 }
