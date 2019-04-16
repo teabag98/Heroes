9dd9a7db5b032fceda8bb899ea7439ce08eee321
eric <eric.kingoo2014@gmail.com>
2019-04-16 10:15:21 +0300

    set port

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,7 +12,15 @@ public class App {
     public static void main(String[] args) {
         String layout = "templates/layout.vtl";
         staticFileLocation("/public");
-          port(8080);
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+          port(port);
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             model.put("template", "templates/index.vtl");
